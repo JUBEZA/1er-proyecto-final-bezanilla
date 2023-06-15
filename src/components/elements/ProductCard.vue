6165b8505638809510862e15f63949148bbf8b42
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-06-15 16:19:44 -0400

    elementos creados con main page y login estilo chore

---
+++ src/components/elements/ProductCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card bg-dark mx-3 my-3 p-3" style="width: 18rem;">
+      <img class="card-img-top" :src="imageUrl" :alt="productName"
+          style="object-fit: cover; width: 100%; aspect-ratio: 1/1;">>
+      <div class="card-body">
+
+          <p class="card-text text-light">{{ productName }}</p>
+          <p class="card-text text-light">{{ productPrice }}</p>
+          <button class="btn btn-light text-dark" @click="innerAddItemToCart"> Add to Cart</button>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ProductCard',
+  props: {
+      imageUrl: String,
+      productName: String,
+      productPrice: Number,
+      productId: Number,
+
+
+  },
+  methods: {
+      innerAddItemToCart(){
+          this.$emit('addItem', this.productId);
+      }
+  }
+}
+
+</script>
+
+<style scoped></style>
---
+++ src/components/views/MainPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+      <h1 class="text-secondary p-5 m-5 bg-black">Choose your weapon and join the band!</h1>
+      <div class="row justify-content-around mx-4">
+          <ProductCard v-for="(item, i) in inventario" :key="i" :imageUrl="item.image" :productName="item.name"
+              :productPrice="item.price" :productId="item.id" @addItem="handleAddItem" />
+
+      </div>
+  </div>
+</template>
+
+<script>
+import ProductCard from '../elements/ProductCard.vue'
+
+
+export default {
+  name: 'HeaderBakery',
+  props: {
+      inventario: {
+          type: Object,
+          required: true
+      }
+
+  },
+
+  components: {
+      ProductCard,
+
+  },
+  methods: {
+      handleAddItem(productId) {
+          this.$emit('itemAdded', productId);
+      },
+  },
+}
+
+</script>
+
+
+<style scoped></style>
